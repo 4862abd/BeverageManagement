dbd2386732c172231d509d06b5391548d17b95b7
bw <4862abd@gmail.com>
2023-05-02 22:46:17 +0900

    [SYSTEM] INIT

---
+++ src/main/java/com/practice/playground_01/Playground01Application.java
@@ -0,0 +1,13 @@
+package com.practice.playground_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Playground01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Playground01Application.class, args);
+    }
+
+}
